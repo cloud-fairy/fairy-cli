80e9c683812126391d919ce78c3f05f4eada2197
eavichay <eavichay@gmail.com>
2022-03-21 18:16:14 +0200

    brew template

--- homebrew/cloudfairy.rb
+++ homebrew/cloudfairy.rb
@@ -1,42 +1,20 @@
-class FairyCli < Formula
+class Fairy < Formula
   desc "cloudfairy cli"
   homepage "https://github.com/cloud-fairy/fairy-cli"
-  version "v0.0.1"
+  sha256 :no_check
+  license "MIT"
+  version: "v0.0.1"
+  
 
-  if Hardware::CPU.is_64_bit?
-    case RbConfig::CONFIG['host_os']
-    when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
-      :windows
-      url "https://github.com/cloud-fairy/fairy-cli/releases/download/v0.0.1/fairy-windows-amd64.exe"
-    when /darwin|mac os/
-      url "https://github.com/cloud-fairy/fairy-cli/releases/download/v0.0.1/fairy-darwin-amd64"
-    when /linux/
-      url "https://github.com/cloud-fairy/fairy-cli/releases/download/v0.0.1/fairy-linux-amd64"
-    when /solaris|bsd/
-      url "https://github.com/cloud-fairy/fairy-cli/releases/download/v0.0.1/fairy-freebsd-amd64"
-      :unix
-    else
-      :unknown
-    end
-  else
-    case RbConfig::CONFIG['host_os']
-    when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
-      :windows
-      url "https://github.com/cloud-fairy/fairy-cli/releases/download/v0.0.1/fairy-windows-386.exe"
-    when /darwin|mac os/
-      :unknown
-    when /linux/
-      url "https://github.com/cloud-fairy/fairy-cli/releases/download/v0.0.1/fairy-linux-386"
-    when /solaris|bsd/
-      url "https://github.com/cloud-fairy/fairy-cli/releases/download/v0.0.1/fairy-freebsd-386"
-      :unix
-    else
-      :unknown
-    end
+  on_macos do
+    url "https://github.com/cloud-fairy/fairy-cli/releases/download/v0.0.1/fairy-darwin-amd64"
+  end
+
+  on_linux do
+    url "https://github.com/cloud-fairy/fairy-cli/releases/download/v0.0.1/fairy-linux-amd64"
   end
 
   def install
     bin.install "fairy"
   end
-
 end
